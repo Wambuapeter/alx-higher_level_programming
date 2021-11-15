6f654f5009e7365c53090636ec94be058d1dac6b
Wambuapeter <peterwambua025@gmail.com>
2021-11-16 00:46:58 +0300

    correcting error

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int check_cycle(listint_t *list);
 
 /**
  * struct listint_s - singly linked list
@@ -18,5 +17,10 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
+int check_cycle(listint_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 #endif
