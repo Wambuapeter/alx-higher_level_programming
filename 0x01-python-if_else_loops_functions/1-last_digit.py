aafe65a5b5c292dc11ff3abeeb71c6107ecdf0b2
Wambuapeter <peterwambua025@gmail.com>
2021-11-16 15:52:17 +0300

    who is greater anyway?

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    string = "and is greater than 5"
 elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    string = "and is 0"
 elif last_digit < 6 and number != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    string = "and is less than 6 and not 0"
+print("Last digit of {} is {} {}".format(number, last_digit, string))
