b841825d1df22f81edf0987c8d793363727adfd4
Wambuapeter <peterwambua025@gmail.com>
2022-01-24 18:25:27 +0300

    Print first x elements

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except IndexError:
-            return printed
+        except (ValueError, TypeError):
+            continue
+    print()
+    return printed
