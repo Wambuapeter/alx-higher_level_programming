a0595d4587c228b940eb1d653d5b2130b6f60d4e
Wambuapeter <peterwambua025@gmail.com>
2022-01-25 21:16:58 +0300

    classes and exceptions lol

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,16 +3,14 @@
 
 
 class Square:
-    '''represent a square'''
-    def __init__(self, size=0):
+    '''represent a square.'''
+    def __init__(self, size):
+        '''
+        args:
+        size(no type)
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-    try:
-        if size != int(size):
-            raise TypeError
-    except TypeError:
-        print("size must be an integer")
-    try:
-        if (size < 0):
-            raise ValueError
-    except ValueError:
-        print("size must be >= 0")
