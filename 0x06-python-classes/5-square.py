efc77dff2d898d00f6e2a55fd6f2937c9b93d31c
Wambuapeter <peterwambua025@gmail.com>
2022-01-25 23:59:07 +0300

    classes lol

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''defines a square.'''
+
+
+class Square:
+    '''represent a square.'''
+    def __init__(self, size=0):
+        '''
+        args:
+        size(of type int).
+        '''
+        self.size = size
+
+        @property
+        def size(self):
+            '''sets current size'''
+            return(self.__size)
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+    def area(self):
+        '''returns area of square'''
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        '''print in stdout square with #'''
+        for i in range(self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
