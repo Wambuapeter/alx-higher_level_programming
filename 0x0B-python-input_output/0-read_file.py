3ef2b313a7646538f6889bc8bcdde9855e85224d
Wambuapeter <peterwambua025@gmail.com>
2022-02-08 14:25:45 +0300

    read that file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+def read_file(filename=""):
+    with open("UTF8") as myFile:
+        read_data = myFile.read()
+
+
+print(myFile)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+Python is cool :)
+Imagine opening and reading files in your code like a boss lol
