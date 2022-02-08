7b7d66d4bcd5023ceded4636bb08f9b9e7c12d3d
Wambuapeter <peterwambua025@gmail.com>
2022-02-08 16:44:13 +0300

    bug debugged

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,7 +4,7 @@
 
 def load_from_json_file(filename):
     '''create an object from json file'''
-    with open(filename, encoding='utf-8') as myFile:
-        my_json_file_contains = read(myFile)
-        object1 = json.loads(my_json_file_contains)
-    return object1
+    with open(filename, encoding='utf-8') as f:
+        my_json_file_content = f.read()
+        created_object = json.loads(my_json_file_content)
+    return created_object
