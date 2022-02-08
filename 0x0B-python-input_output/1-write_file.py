97ce6f2d91e7c506622c666c6567fed38d4e6a70
Wambuapeter <peterwambua025@gmail.com>
2022-02-08 16:29:07 +0300

    write that file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """write and return length of string written"""
-    with open(filename, mode="w" encoding="UTF8") as myFile:
+    with open(filename, mode="w" encoding="utf-8") as myFile:
         return myFile.write(text)
