094faf79b025b956b40c90cef5e9d3e12e66dec3
Wambuapeter <peterwambua025@gmail.com>
2022-02-08 16:34:48 +0300

    bug debugged

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''appends text to a file'''
+"""appends text to a file"""
 
 
 def append_write(filename="", text=""):
-    '''append at the end the text'''
-    with open(filename, encoding="UTF8") as myFile:
+    """append at the end the text"""
+    with open(filename, encoding="utf-8") as myFile:
         return myFile.append(text)
