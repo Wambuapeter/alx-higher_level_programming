1ed263abb9fb6596a81882b2331dc22e1caa2b3c
Wambuapeter <peterwambua025@gmail.com>
2022-02-08 16:24:38 +0300

    read that file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     '''use with to automatically close'''
-    with open(filename, encoding="UTF8") as myFile:
+    with open(filename, encoding="utf-8") as myFile:
         read_data = myFile.read()
-        print(myFile, end="")
+        print(read_data, end="")
