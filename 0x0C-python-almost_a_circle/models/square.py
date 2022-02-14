fd3f3e36d51fb13d868466f99d94c22c54f78cbe
Wambuapeter <peterwambua025@gmail.com>
2022-02-14 21:57:55 +0300

    almost a circle indeed :(

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,43 +1,51 @@
 #!/usr/bin/python3
 
+"""this class Square inherits from Rectangle"""
+
 from models import rectangle
 Rectangle = rectangle.Rectangle
 
 
 class Square(Rectangle):
-    '''inheritrs from class Rectangle'''
+
+    """Calls the super class with id, x, y, width and height"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        '''calls the super class'''
+
+        """calls the super class"""
+
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+
+        """the overloading method"""
+
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        return self.size
+        """get size"""
+        return self.width
 
     @size.setter
-    '''sets width and height to same value'''
-    def size(self, value):
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        '''the overloading method'''
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
-            self.id, self.x, self.y, self.size)
+    def size(self, size):
+        """sets width and height to same value"""
+        self.width = size
+        self.height = size
 
     def update(self, *args, **kwargs):
-        '''updates the private attributes'''
-        square_attr = [id, size, x, y]
+        """updates the private attribute"""
+        square_attrs = ["id", "size", "x", "y"]
         if args is None or len(args) == 0:
-            for key, value in kwargs.items():
-                if key in square_attr:
-                    setattr(self, key, value)
+            for k, v in kwargs.items():
+                setattr(self, k, v)
         else:
             for i in range(len(args)):
-                setattr(self, square_attr[i], args[i])
+                setattr(self, square_attrs[i], args[i])
 
     def to_dictionary(self):
-        '''returns dictionary representation of the square'''
-        square_dic = {'id': self.id, 'size': self.size,
-                      'x': self.x, 'y': self.y}
-        return square_dic
+        """returns the dictionary representation of a Square"""
+        square_dict = {'id': self.id, 'size': self.size,
+                       'x': self.x, 'y': self.y}
+        return square_dict
