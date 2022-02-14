32a9c009bd59f1db453557787c1d8651a95c7284
Wambuapeter <peterwambua025@gmail.com>
2022-02-14 21:39:47 +0300

    almost a circle indeed :(

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-'''class Rectangle that inherits class Base'''
+"""class Rectangle that inherits class Base"""
 
 from models import base
 Base = base.Base
 
 
 class Rectangle(Base):
-    '''private instance attributes with public getter and setter'''
+    """private instance attributes with public getter and setter"""
     def __init__(self, width, height, x=0, y=0, id=None):
 
         super().__init__(id)
@@ -33,12 +33,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        '''get height'''
+        """get height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''set height'''
+        """set height"""
         if not isinstance(self.height, int):
             raise TypeError("height must be an integer")
         if self.height <= 0:
@@ -47,12 +47,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        '''get x'''
+        """get x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''set x'''
+        """set x"""
         if not isinstance(self.x, int):
             raise TypeError("x must be an integer")
         if self.x < 0:
@@ -61,12 +61,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        '''get y'''
+        """get y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''set y'''
+        """set y"""
         if not isinstance(self.y, int):
             raise TypeError("y must be an integer")
         if self.y < 0:
@@ -74,22 +74,22 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        '''returns area value of the Rectangle instance'''
+        """returns area value of the Rectangle instance"""
         return (self.__width * self.__height)
 
     def display(self):
-        '''prints in stdout the rectangle instance with #'''
+        """prints in stdout the rectangle instance with #"""
         print(("\n" * self.__y) +
               "\n".join(((" " * self.__x) + ("#" * self.__width))
                         for i in range(self.__height)))
 
     def __str__(self):
-        '''returns some specified value'''
+        """returns some specified value"""
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
             self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
-        '''updates the private attributes'''
+        """updates the private attributes"""
         rect_attr = [id, width, height, x, y]
         if args is None or len(args) == 0:
             for key, value in kwargs.items():
@@ -100,7 +100,7 @@ class Rectangle(Base):
                 setattr(self, rect_attr[i], args[i])
 
     def to_dictionary(self):
-        '''returns dictionary representation of the reactangle'''
+        """returns dictionary representation of the reactangle"""
         rect_dict = {'id': self.id, 'width': self.__width,
                      'height': self.__height,
                      'x': self.__x, 'y': self.__y}
